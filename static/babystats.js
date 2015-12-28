29f0d7600c4c1a90cee91e8478ca183c9d4a2f37
Ian Gulliver <ian@flamingcow.tv>
2015-12-27 19:50:37 -0800

    Clean up layout deferral. Require login.

--- static/babystats.js
+++ static/babystats.js
@@ -24,20 +24,32 @@ var BabyStats = function(container) {
   this.intervals_ = {};
 
   this.cosmo_ = new Cosmopolite();
-  hogfather.PublicChat.Create(this.cosmo_).then(function(chat) {
-    this.chat_ = chat;
+  if (window.location.hash) {
+    var id = window.location.hash.substring(1);
+    hogfather.PublicChat.Join(this.cosmo_, id).then(
+        this.onChatReady_.bind(this));
+  } else {
+    hogfather.PublicChat.Create(this.cosmo_).then(
+        this.onChatReady_.bind(this));
+  }
+};
 
-    this.buildCells_();
-    this.buildStylesheet_();
-    this.buildLayout_();
 
-    window.addEventListener('resize', this.rebuildIfNeeded_.bind(this));
+BabyStats.prototype.onChatReady_ = function(chat) {
+  this.chat_ = chat;
 
-    var grid = this.calculateGrid_();
-    this.gridWidthCells_ = grid.gridWidthCells;
-    this.gridHeightCells_ = grid.gridHeightCells;
-    this.buildGrid_();
-  }.bind(this));
+  window.location.hash = this.chat_.getID();
+
+  this.buildCells_();
+  this.buildStylesheet_();
+  this.buildLayout_();
+
+  window.addEventListener('resize', this.rebuildIfNeeded_.bind(this));
+
+  var grid = this.calculateGrid_();
+  this.gridWidthCells_ = grid.gridWidthCells;
+  this.gridHeightCells_ = grid.gridHeightCells;
+  this.buildGrid_();
 };
 
 
